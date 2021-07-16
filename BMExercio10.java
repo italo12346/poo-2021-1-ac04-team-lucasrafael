4c38ac1a9d3c7e85c7e93dedc6209e7f31deccca
Lucas <76121589+Lucasotaro@users.noreply.github.com>
2021-07-15 22:00:13 -0300

    Adição da BMExercicio10

---
+++ BMExercio10.java
@@ -0,0 +1,10 @@
+public class BMExercio10{
+  public static void main(String[] args){
+    int num = 10;
+    int cont = 0;
+    do{
+      System.out.println(num + cont);
+      cont ++;
+    }while(cont <= 20);
+  }
+}
