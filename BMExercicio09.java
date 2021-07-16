30796f4058e0828601b4794756d4607ac7f1424a
Rafael Barroso <76064049+Raafa81@users.noreply.github.com>
2021-07-15 21:59:59 -0300

    Adição da BMExercicio09

---
+++ BMExercicio09.java
@@ -0,0 +1,10 @@
+class BMExercio09 {
+  public static void main(String[] args){
+    int num = 10;
+    int cont = 0;
+    while(cont <= 20){
+      System.out.println(num + cont);
+      cont++;
+    }
+  }
+}
