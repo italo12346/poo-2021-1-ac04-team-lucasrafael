6ee7d2ed2716fd4de02cfa46eeae540b1d6db961
Rafael Barroso <76064049+Raafa81@users.noreply.github.com>
2021-07-15 21:57:02 -0300

    Adição da CBBAtividadePratica5

---
+++ CBBAtividadePratica5.java
@@ -0,0 +1,9 @@
+class CBBAtividadePratica5 {
+  public static void main(String[] args){
+  for(int i = 0; i <= 100; i++){
+    if(i % 2 == 0 && i != 0){
+      System.out.println(i);
+      }
+    }
+  }
+}
